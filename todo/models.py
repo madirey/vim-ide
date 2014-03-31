afcce75943320317e0087c0b836a85761dde1eb7
Matt Caldwell <matt.caldwell@gmail.com>
2014-03-30 22:08:27 -0400

    more incremental updates

--- todo/models.py
+++ todo/models.py
@@ -1,4 +1,6 @@
 from django.db import models
 
 class Todo(models.Model):
-    pass
+    text = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    due = models.DateTimeField()
--- todo/urls.py
+++ todo/urls.py
@@ -4,9 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'todo.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'todo.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
